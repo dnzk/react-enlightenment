bd2a496c50af59e37f0bb93ca2cc285874a7359e
danish <danish.kalesaran@invoice2go.com>
2016-08-14 21:48:19 +0700

    make react work

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 require('file?name=[name].[ext]!./index.html');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+        <h1>React.js is awesome</h1>
+        );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
